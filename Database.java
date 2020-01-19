90852eae82bbc8cbaefa771b66b9143b339aa179
sophiaju <60048531+sophiaju@users.noreply.github.com>
2020-01-18 21:09:16 -0500

    Add files via upload

--- Database.java
+++ Database.java
@@ -1,4 +1,7 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.*;
+import java.lang.Math;
 
 public class Database {
     private ArrayList<Produce> db;
@@ -9,26 +12,26 @@ public class Database {
     }
 
     public void loadDB() {
-        Produce a = new Produce("apple", 3306.3, "Washington State");
-        Produce b = new Produce("pear", 3306.3, "Washington State");
-        Produce c = new Produce("orange", 1725.07, "Florida");
-        Produce d = new Produce("grapefruit", 1725.07, "Florida");
-        Produce e = new Produce("lemon", 1725.07, "Florida");
-        Produce f = new Produce("lime", 1725.07, "Florida");
-        Produce g = new Produce("clementine", 1725.07, "Florida");
-        Produce h = new Produce("tangerine", 1725.07, "Florida");
-        Produce i = new Produce("cabbage", 3573.63, "California");
-        Produce j = new Produce("lettuce", 3573.63, "California");
-        Produce k = new Produce("grapes", 3573.63, "California");
-        Produce l = new Produce("avocado", 3482.03, "Michoacan, Mexico");
-        Produce m = new Produce("watermelon", 3482.03, "Michoacan, Mexico");
-        Produce n = new Produce("mango", 3482.03, "Michoacan, Mexico");
-        Produce o = new Produce("banana", 3893.54, "Guatemala");
-        Produce p = new Produce("tomato", 3482.03, "Michoacan, Mexico");
-        Produce q = new Produce("melon", 3573.63, "California");
-        Produce r = new Produce("soybean", 1502.14, "Iowa");
-        Produce s = new Produce("onion", 3573.63, "California");
-        Produce t = new Produce("potato", 3009.73, "Idaho");
+        Produce a = new Produce("apple", 3306, "Washington State");
+        Produce b = new Produce("pear", 3306, "Washington State");
+        Produce c = new Produce("orange", 1725, "Florida");
+        Produce d = new Produce("grapefruit", 1725, "Florida");
+        Produce e = new Produce("lemon", 1725, "Florida");
+        Produce f = new Produce("lime", 1725, "Florida");
+        Produce g = new Produce("clementine", 1725, "Florida");
+        Produce h = new Produce("tangerine", 1725, "Florida");
+        Produce i = new Produce("cabbage", 3573, "California");
+        Produce j = new Produce("lettuce", 3573, "California");
+        Produce k = new Produce("grapes", 3573, "California");
+        Produce l = new Produce("avocado", 3482, "Michoacan, Mexico");
+        Produce m = new Produce("watermelon", 3482, "Michoacan, Mexico");
+        Produce n = new Produce("mango", 3482, "Michoacan, Mexico");
+        Produce o = new Produce("banana", 3893, "Guatemala");
+        Produce p = new Produce("tomato", 3482, "Michoacan, Mexico");
+        Produce q = new Produce("melon", 3573, "California");
+        Produce r = new Produce("soybean", 1502, "Iowa");
+        Produce s = new Produce("onion", 3573, "California");
+        Produce t = new Produce("potato", 3009, "Idaho");
 
         this.db.add(a);
         this.db.add(b);
@@ -66,7 +69,43 @@ public class Database {
                 return p;
             }
         }
-        Produce other = new Produce("other", 3257.06, "Average Estimate based on database");
-        return other;
+        return null;
+    }
+
+    public Produce addNew(String name, String origin) throws FileNotFoundException{
+        //search for country in database
+        double carbonOut;
+        double mtlLati = 45.508800;
+        double mtlLongi = -73.571750;
+        String fileName = "country_coordinates.csv";
+        File file = new File(fileName);
+
+        Scanner inputStream = new Scanner(file);
+        while(inputStream.hasNext()){
+            String data = inputStream.next();
+            String[] split = data.split(",");
+            String country = split[3];
+            if(country.equalsIgnoreCase(origin)){
+                String lat = split[1];
+                String lon = split[2];
+                double lati = Double.parseDouble(lat);
+                double longi = Double.parseDouble(lon);
+
+                double dist = 2*6371*Math.asin(Math.sqrt(Math.pow((Math.sin((lati*(Math.PI/180.0)-mtlLati*(Math.PI/180))/2)), 2)+Math.cos(lati*(Math.PI/180))*Math.cos(mtlLati*(Math.PI/180))*Math.pow(Math.sin(((longi*(Math.PI/180)-mtlLongi*(Math.PI/180))/2)),2)));
+                double kgC;
+                if(country.equalsIgnoreCase("United-States")||country.equalsIgnoreCase("Mexico")){
+                     kgC = 0.9*dist;
+                } else {kgC=15*dist;}
+
+
+                Produce newP = new Produce(name, kgC, origin);
+                db.add(newP);
+                return newP;
+
+
+            }
+        }
+        System.out.println("Country not found");
+        return null;
     }
 }
--- Main.java
+++ Main.java
@@ -2,20 +2,76 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main (String args[]) {
+    public static void main (String args[]) throws Exception {
 
         Database db = new Database();
         db.loadDB();
 
         Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
 
-        Produce p = db.find(input);
-        double kgCarbon = p.getKgCarbon();
-        String origin = p.getOrigin();
+        System.out.print("Enter s to search, a to add: ");
+        String in = sc.nextLine();
 
-        System.out.println(kgCarbon);
-        System.out.println(origin);
+        if(in.equalsIgnoreCase("s")){
+            System.out.print("Enter the name of your produce: ");
+            String input = sc.nextLine();
+
+            Produce p = db.find(input);
+            if(p==null){
+                System.out.println("Produce not found, please enter the following information to the database.");
+
+                System.out.print("Enter the name of the produce: ");
+
+                String newName = sc.nextLine();
+
+                System.out.print("Enter the country of origin. Replace all spaces with '-': " );
+
+                String newOrigin = sc.nextLine();
+
+                Produce newP = db.addNew(newName, newOrigin);
+                double newKgC = newP.getKgCarbon();
+
+                System.out.println("Thank you! Your entry has been successfully added to the database.");
+
+                System.out.println("Approximately " +  (int) newKgC + " kg of carbon dioxide were released from shipment of this product.");
+
+                int newTree = (int) (newKgC/22.0);
+                System.out.println("It would take about " + newTree + " trees one year to offset this amount of carbon dioxide.");
+                return;
+            }
+            double kgCarbon = p.getKgCarbon();
+            String origin = p.getOrigin();
+
+            System.out.println("This product came from: " + origin);
+            System.out.println("Approximately " +  kgCarbon + " kg of carbon dioxide were released from shipment of this product.");
+
+
+            int tree = (int) (kgCarbon/22.0);
+            System.out.println("It would take about " + tree + " trees one year to offset this amount of carbon dioxide.");
+
+        } else if(in.equalsIgnoreCase("a")){
+            System.out.print("Enter the name of the produce: ");
+
+            String newName = sc.nextLine();
+
+            System.out.print("Enter the country of origin. Replace all spaces with '-': " );
+
+            String newOrigin = sc.nextLine();
+
+            Produce newP = db.addNew(newName, newOrigin);
+            double newKgC = newP.getKgCarbon();
+
+            System.out.println("Thank you! Your entry has been successfully added to the database.");
+
+            System.out.println("Approximately " +  (int) newKgC + " kg of carbon dioxide were released from shipment of this product.");
+
+            int newTree = (int) (newKgC/22.0);
+            System.out.println("It would take about " + newTree + " trees one year to offset this amount of carbon dioxide.");
+
+        } else{
+            System.out.println("Please enter 's' or 'a'");
+        }
 
     }
+
 }
